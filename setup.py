68a18875ce1dfd67b39b0c144296a8e6bd97baf4
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2017-12-25 12:02:30 -0500

    Removes NLTK installation from setup.py.

--- setup.py
+++ setup.py
@@ -14,31 +14,7 @@ For more information on this package, see
 
 """
 from setuptools import setup
-from setuptools.command.install import install as _install
-import sys
-
-
-class install(_install):
-    """Installs the necessary NLTK data after the installation of this package
-    has completed.
-
-    """
-    def run(self):
-        """Installs the CMU pronunciation dictionary data from NLTK by running
-        the command-line NLTK downloader.
-
-        """
-        import subprocess
-        super().run()
-        try:
-            subprocess.call(['python', '-m', 'nltk.downloader', 'cmudict'])
-        except OSError:
-            print('Failed to download CMU pronunciation dictionary data.')
-            print('Maybe try running:')
-            print('  python -c "import nltk; nltk.download()".')
-            sys.exit(-1)
 
 setup(
-    cmdclass={'install': install},
     long_description=__doc__,
 )
